3b83f57ded7712e984f5ab237eb25075a9f9a576
keolala <keola.mahatma16@gmail.com>
2025-06-26 23:50:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-// Display an alert when the page loads
 window.addEventListener("DOMContentLoaded", function () {
   alert("Welcome to my first web page!");
-});
 
-// Change button text on click
-document.getElementById("surprise").addEventListener("click", function () {
-  this.textContent = "Tell Tom Holland";
+  document.getElementById("surprise").addEventListener("click", function () {
+    this.textContent = "Tell Tom Holland";
+  });
 });
